eb2e09bf63b629b8b8a2957b9f07987a37eeca81
Darks0rr0w <z1msada@gmail.com>
2015-09-02 09:46:21 +0300

    email

--- index.php
+++ index.php
@@ -14,7 +14,29 @@ if (isset($_POST['days'])){
 
 function sendMail(PDO $conn, $row, $days)
 {
-    //Sends message to email with all info about article and $days var ;
+
+
+    $current_time = new DateTime();
+    $published_to = getEmailReceived($conn, $row['id']);
+    $difference  = $published_to->diff($current_time)->days;
+    if ($difference == 5 ||
+        $difference == 2 ||
+        $difference == 1){
+        emailReceived($conn,$row['id']);
+        //Sends message to email with all info about article and $days var ;
+    }
+
+}
+
+function emailReceived(PDO $conn, $id)
+{
+    $email_received = new DateTime();
+    $email_received = $email_received->format("Y-m-d H:i:s");
+    $query = "UPDATE items SET email_received = :email_received WHERE id = :id";
+    $stmt = $conn->prepare($query);
+    $stmt->bindParam('email_received', $email_received);
+    $stmt->bindParam('id', $id);
+    $stmt->execute();
 }
 
 function getPublishedTo(PDO $conn, $id)
@@ -53,14 +75,15 @@ function days(PDO $conn, $id)
 }
 function main(PDO $conn)
 {
-    //$offset
-    $rows = findAllItems();
+    $limit = 100;
+    $rows = findAllItems($conn, $limit);
     foreach ($rows as $row)
     {
         if (days($conn, $row['id']) == 5 ||
             days($conn, $row['id']) == 2 ||
             days($conn, $row['id']) == 1){
             sendMail($conn, $row, days($conn, $row['id']));
+
         }
 
     }
@@ -81,8 +104,21 @@ function publish(PDO $conn, $id)
 
 }
 
-function findAllItems(PDO $conn, $limit, $offset)
+function getEmailReceived(PDO $conn, $id)
 {
+    $query = "SELECT  email_received FROM items WHERE id = :id";
+    $stmt = $conn->prepare($query);
+    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+    $stmt->execute();
+    $date = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    return new DateTime($date['publicated_to']);
+}
+
+function findAllItems(PDO $conn, $limit)
+{
+
+
     $query = "SELECT * FROM items LIMIT :limit OFFSET :offset";
     $stmt = $conn->prepare($query);
     $stmt->bindParam(':limit', $limit, PDO::PARAM_INT);
@@ -126,7 +162,6 @@ function findAllItems(PDO $conn, $limit, $offset)
         <form action="index.php?id=<?=$_GET['id']?>" method="post">
             Enter number of days:
             <input type="text" name="days">
-            <input type="hidden" name="id" value="">
             <input type="submit" name="submit" value="Submit">
         </form>
 
